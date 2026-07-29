356495953c8325516469c72073a4697022c61416
lavishtembhare <lavishtembhare93@gmail.com>
2026-07-29 16:18:03 +0530

    Initial commit

---
+++ src/main/java/com/main/LinkSphere_Backend/LinkSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.main.LinkSphere_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkSphereBackendApplication.class, args);
+	}
+
+}
