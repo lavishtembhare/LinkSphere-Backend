2fc243bef288cca3cf94c9dab4bc1e534fed1b91
lavishtembhare <lavishtembhare93@gmail.com>
2026-08-01 22:39:44 +0530

    Update

--- src/main/java/com/main/LinkSphere_Backend/models/UrlMapping.java
+++ src/main/java/com/main/LinkSphere_Backend/models/UrlMapping.java
@@ -1,4 +1,22 @@
 package com.main.LinkSphere_Backend.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount=0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
