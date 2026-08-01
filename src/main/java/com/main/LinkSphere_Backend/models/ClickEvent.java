a29920fa6294d2491832c3ee22979fa9b353faf6
lavishtembhare <lavishtembhare93@gmail.com>
2026-08-02 00:18:13 +0530

    Update

--- src/main/java/com/main/LinkSphere_Backend/models/ClickEvent.java
+++ src/main/java/com/main/LinkSphere_Backend/models/ClickEvent.java
@@ -1,4 +1,15 @@
 package com.main.LinkSphere_Backend.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
 public class ClickEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+    @ManyToOne
+    @JoinColumn(name = "url_mapping_id")
+    private UrlMapping urlMapping;
 }
