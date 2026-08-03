9f72ec995873abf0339dba2894f0341429dddc83
lavishtembhare <lavishtembhare93@gmail.com>
2026-08-03 14:03:10 +0530

    Use PostgreSQL; add Security & JWT deps
    Replaced MySQL connector with the PostgreSQL driver and added Spring Security + test deps. Added jjwt (api/impl/jackson) dependencies for JWT support. Marked ClickEvent as a JPA entity and added Lombok @Data. Updated application.properties with PostgreSQL datasource settings, driver, and JPA options. Enabled default annotation processing profile in .idea/compiler.xml. Note: target/classes/application.properties was updated accordingly.

--- src/main/java/com/main/LinkSphere_Backend/models/ClickEvent.java
+++ src/main/java/com/main/LinkSphere_Backend/models/ClickEvent.java
@@ -1,9 +1,12 @@
 package com.main.LinkSphere_Backend.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Data
 public class ClickEvent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
