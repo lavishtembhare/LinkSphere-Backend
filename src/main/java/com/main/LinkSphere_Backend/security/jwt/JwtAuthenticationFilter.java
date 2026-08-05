6a476ce59e3d8e854ad85cbbc77fc6cfeb2b1057
lavishtembhare <lavishtembhare93@gmail.com>
2026-08-06 00:57:50 +0530

    Json Web Token Implemented

---
+++ src/main/java/com/main/LinkSphere_Backend/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.main.LinkSphere_Backend.security.jwt;
+
+import com.main.LinkSphere_Backend.sevice.UserDetailsImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtTokenProvider;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String jwt=jwtTokenProvider.getJwtFromHeader(request);
+            if (jwt!=null&&jwtTokenProvider.validateToken(jwt)){
+                String username=jwtTokenProvider.getUserFromJwtToken(jwt);
+                UserDetails userDetails= userDetailsService.loadUserByUsername(username);
+                if(userDetails!=null){
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/main/LinkSphere_Backend/security/jwt/JwtAuthenticationResponse.java
@@ -0,0 +1,9 @@
+package com.main.LinkSphere_Backend.security.jwt;
+
+import lombok.Data;
+
+@Data
+public class JwtAuthenticationResponse {
+    private String token;
+
+}
